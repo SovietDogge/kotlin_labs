835ef52bd01677b331423e428e7b458cf229071e
Nikita Vanda <114001059+SovietDogge@users.noreply.github.com>
2024-03-20 21:42:47 +0300

    Update and rename main.kt to lab2.kt

--- app/src/main/java/com/example/myapplication/lab2.kt
+++ app/src/main/java/com/example/myapplication/lab2.kt
@@ -1,25 +1,19 @@
 package com.example.myapplication
 
-// Parent class
 open class Item(val name: String, val price: Double) {
-    // Method to calculate the cost
     open fun calculateCost(): Double {
         return price
     }
 }
 
-// Subclass for products
 class Product(name: String, price: Double, val weight: Double) : Item(name, price) {
-    // Overridden method to calculate the cost
     override fun calculateCost(): Double {
         return price * weight
     }
 }
 
-// Subclass for electronics
 class Electronics(name: String, price: Double, val warranty: Int) : Item(name, price)
 
-// Subclass for clothing
 class Clothing(name: String, price: Double, val size: String) : Item(name, price)
 
 fun main() {
